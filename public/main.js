39b8a401d62f7b46f298b2571e0869f90b6b6960
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-21 11:37:45 -0400

    code

--- public/main.js
+++ public/main.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("./db");
 function App() {
     var button = document.getElementById("button");
     button.addEventListener("click", function () {
@@ -10,15 +7,11 @@ function App() {
         if (value === guess) {
             input.style.backgroundColor = "green";
             getDocument("demo", "The Number: " + value + " is Correct");
-            (0, db_1.AppsClient)("The Number: " + value + " is Correct").then(function (e) {
-                return console.log(e);
-            });
         }
         else {
             input.style.backgroundColor = "red";
             getDocument("demo", "The Number: " + value + " is Not Correct");
             getDocument("demo", "The Number: " + guess.toString() + " Was The Guess");
-            (0, db_1.AppsClient)("The Number: " + guess.toString() + " Was The Guess").then(function (e) { return console.log(e); });
         }
     });
 }
@@ -26,7 +19,4 @@ App();
 function getDocument(doc, text) {
     var textContent = document.getElementById(doc);
     textContent.textContent = text;
-}
-function AppClient() {
-    throw new Error("Function not implemented.");
 }
--- public/main.ts
+++ public/main.ts
@@ -1,5 +1,3 @@
-import { AppsClient } from "./DB/db";
-
 function App() {
   const button = document.getElementById("button") as HTMLButtonElement;
 
@@ -10,17 +8,10 @@ function App() {
     if (value === guess) {
       input.style.backgroundColor = "green";
       getDocument("demo", "The Number: " + value + " is Correct");
-      AppsClient("The Number: " + value + " is Correct").then((e) =>
-        console.log(e)
-      );
     } else {
       input.style.backgroundColor = "red";
       getDocument("demo", "The Number: " + value + " is Not Correct");
       getDocument("demo", "The Number: " + guess.toString() + " Was The Guess");
-
-      AppsClient("The Number: " + guess.toString() + " Was The Guess").then(
-        (e) => console.log(e)
-      );
     }
   });
 }
@@ -30,7 +21,4 @@ App();
 function getDocument(doc: string, text: string) {
   const textContent = document.getElementById(doc) as HTMLElement;
   textContent.textContent = text;
-}
-function AppClient() {
-  throw new Error("Function not implemented.");
 }
